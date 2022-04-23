6f26ad5cb3db4c3c96004ea989b4680a2df27d2c
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-23 18:01:15 -0400

    commit tree 32ee92d9e3854c0fb53cf739dc4383f0dd202116

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+//declares variable
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'Micky';
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (customerName) {
+    bestCustomer = customerName;
+}
+
+function changeLeastFavoriteCustomer (customerName) {
+    leastFavoriteCustomer = customerName;
+}
+
